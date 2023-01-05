f544f0803238a59b3243ed36da2f2dc243bdeb21
Faruk Hossain Milon <milonju01@gmail.com>
2023-01-04 23:38:55 -0800

    Time: 4 ms (79.78%), Space: 8.1 MB (63.71%) - LeetHub

---
+++ 0249-group-shifted-strings/0249-group-shifted-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    string shiftString(string str) {
+        int len = str.size();
+        int dist = str[0] - 'a';
+        for (auto &ch : str) {
+            ch = (char)((ch - 'a' - dist + 26) % 26 + 'a');
+        }
+        return str;
+    }
+    
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> mp;
+        for (auto &str : strings) {
+            string shiftedStr = shiftString(str);
+            mp[shiftedStr].push_back(str);
+        }
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
