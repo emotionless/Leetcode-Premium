94d686f2ffb194eb8e4051bc1a4b3b48c75b25e9
Faruk Hossain Milon <milonju01@gmail.com>
2023-01-10 10:55:13 +0600

    Time: 16 ms (40.13%), Space: 10.2 MB (20.17%) - LeetHub

---
+++ 0256-paint-house/0256-paint-house.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        vector<vector<int>> dp(n, vector<int>(3, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < 3; j++) {
+                int mn = INT_MAX;
+                for (int k = 0; k < 3; k++) {
+                    if (j == k) continue;
+                    if ((i+1) == n) continue;
+                    mn = min(mn, dp[i+1][k]);
+                }
+                dp[i][j] = (mn == INT_MAX? 0 : mn) + costs[i][j];
+            }
+        }
+        return min(dp[0][0], min(dp[0][1], dp[0][2]));
+    }
+};
