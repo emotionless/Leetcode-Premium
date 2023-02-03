b593e38732ce0c4bc30dda836173bd3111f9b55e
Faruk Hossain Milon <milonju01@gmail.com>
2023-02-03 14:55:32 +0600

    Time: 445 ms (17.43%), Space: 87.4 MB (9.17%) - LeetHub

---
+++ 0294-flip-game-ii/0294-flip-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canWin(string currentState) {
+        if (currentState.size() <= 1) return false;
+        
+        for (int i = 0; i < currentState.size() - 1; i++) {
+            if (currentState[i] == '+' && currentState[i+1] == '+') {
+                string str = currentState;
+                str[i] = str[i+1] = '-';
+                if (!canWin(str)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
